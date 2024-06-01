bc141e93a0ed06d7c6b6042fcba50b6236e6ceae
Kaushiki Bhattacharyya <141013087+itsKaushiki@users.noreply.github.com>
2024-06-01 15:07:46 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.get("/" ,(req,res)=>{
+    res.json("hello");
+})
 
 //routes
 readdirSync('./routes').map((route)=> app.use('/api/v1',require('./routes/'+ route)))
